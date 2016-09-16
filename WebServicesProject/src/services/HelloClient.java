9cd4a1ab3bc5bbe964cb41d653433e38e33a2b9e
Marti_2203 <marti_2203@abv.bg>
2016-09-16 21:58:22 +0300

    Added "/hello" to HelloClient

--- WebServicesProject/src/services/HelloClient.java
+++ WebServicesProject/src/services/HelloClient.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class HelloClient
  */
-@WebServlet(urlPatterns = "/helloClient" )
+@WebServlet(urlPatterns = {"/helloClient", "/hello"} )
 public class HelloClient extends HttpServlet
 	{
 		private static final long serialVersionUID = 1L;
